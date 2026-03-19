3b24615e2ac3dcf95790c67730c0996c8e49190d
Shuvoh715 <etir20877@gmail.com>
2026-03-20 01:40:11 +0600

    Update chessboard.js

--- chessboard.js
+++ chessboard.js
@@ -1,25 +1,26 @@
-function Chessboard(id, config) {
-  const board = document.getElementById(id);
-  board.innerHTML = "";
+var board = null;
+var game = new Chess();
 
-  const squares = [];
+function initBoard() {
+  var config = {
+    draggable: true,
+    position: 'start',
+    onDrop: onDrop
+  };
 
-  for (let i = 0; i < 64; i++) {
-    const square = document.createElement("div");
-    square.style.width = "12.5%";
-    square.style.height = "50px";
-    square.style.float = "left";
-    square.style.boxSizing = "border-box";
-
-    if ((Math.floor(i / 8) + i) % 2 === 0) {
-      square.style.background = "#f0d9b5";
-    } else {
-      square.style.background = "#b58863";
-    }
-
-    board.appendChild(square);
-    squares.push(square);
-  }
-
-  return {};
+  board = Chessboard('board', config);
 }
+
+function onDrop(source, target) {
+  var move = game.move({
+    from: source,
+    to: target,
+    promotion: 'q'
+  });
+
+  if (move === null) return 'snapback';
+}
+
+window.onload = function () {
+  initBoard();
+};
